c42346a4148a1e5bd04826efbe4bed3ef6a1b0c4
Scott Wales <scott.wales@unimelb.edu.au>
2015-04-09 16:47:38 +1000

    Follow redirects

--- local/server/manifests/proxy/connection.pp
+++ local/server/manifests/proxy/connection.pp
@@ -37,7 +37,7 @@ define server::proxy::connection (
   client::icinga::check {"https-${escaped_name}":
     display_name     => "https://${vhost}${name}",
     nrpe_plugin      => 'check_http',
-    nrpe_plugin_args => "-H '${vhost}' -u '${name}' --ssl",
+    nrpe_plugin_args => "-H '${vhost}' -u '${name}' --ssl -f follow -w 1 -c 10",
   }
 
   apacheplus::location {$name:
